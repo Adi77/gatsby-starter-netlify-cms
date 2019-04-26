a4f99b50daca972b6572e0960691d88fecd42441
Adrian Felder <adrianfelder@gmx.ch>
2019-04-26 20:00:33 +0200

    added tpv-products fields to config

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -5,7 +5,11 @@ import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
+import TpvProductPagePreview from './preview-templates/TpvProductPagePreview'
+
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+
+CMS.registerPreviewTemplate('tpv-products', TpvProductPagePreview)
