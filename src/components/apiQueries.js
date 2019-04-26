617ca616ea9e8ee5aeac6afa752342ea7c643c8d
Adrian Felder <adrianfelder@gmx.ch>
2019-04-26 18:25:25 +0200

    initial

---
+++ src/components/apiQueries.js
@@ -0,0 +1,48 @@
+import { graphql } from "gatsby";
+
+  export const query = graphql`
+  fragment ApiQueriesFragment on Query {
+    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+      frontmatter {
+        title
+        image {
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        heading
+        subheading
+        mainpitch {
+          title
+          description
+        }
+        description
+        intro {
+          blurbs {
+            image {
+              childImageSharp {
+                fluid(maxWidth: 240, quality: 64) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+            text
+          }
+          heading
+          description
+        }
+      }
+    }
+    aboutPageData: allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            title
+          }
+        }
+      }
+    }
+  }
+  `;
